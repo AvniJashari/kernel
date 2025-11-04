55faa9c6bab4e898f9d615203f0afc80b9c3ad32
myError <avni.jashari@proton.me>
2025-11-04 11:14:24 +0100

    primo commit, inizializazione del kernel
    Per ora il kernel non fa niente, va solo in un ciclo loop e dentro
    rimana in attesa (comando asm wfi "wait for interrupt")

---
+++ README.md
@@ -0,0 +1,10 @@
+# KERNEL
+
+il progetto cerca di creare un minikernel su architetura RISC V a 64 bit, per la virtualizazione uso QEMU. per far copilare e far partire QEMU basta usare il file run.sh
+
+## dipendenze
+rust, con scaricato anche riscv64gc-unknown-none-elf
+QEMU, con la virtualizazione per architeture RISC V 64bit
+
+## TODO
+non usare piu' il file run.sh, ma usare le funzionalità di Cargo
---
+++ src/main.rs
@@ -0,0 +1,61 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+#[macro_use]
+
+
+
+#[inline(always)]
+fn do_nothing() -> ! {
+    loop {
+        unsafe {
+            core::arch::asm!("wfi");
+        }
+    }
+}
+
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text.boot")]
+pub extern "C" fn boot() -> ! {
+    unsafe {
+        asm!(
+            "la sp, __stack_top",  // carica il puntatore a stack top in sp(stack poiter)
+            "j {main}",            // Jump verso il main
+            main = sym main,       // definisco main a livello di assembly
+            options(noreturn)      // questa funzione non fa return
+        );
+    }
+}
+
+
+unsafe extern "C" {
+    static mut __bss: u8;
+    static mut __bss_end: u8;
+}
+
+fn main() -> ! {
+    unsafe {set_bss_to_zero();}
+
+    do_nothing();
+}
+
+unsafe fn set_bss_to_zero() -> (){
+    let bss_start = &raw mut __bss;
+    let bss_size = (&raw mut __bss_end as usize) - (&raw mut __bss as usize);
+    core::ptr::write_bytes(bss_start, 0, bss_size);
+}
+
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+pub fn panic_handler(info: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            core::arch::asm!("wfi"); // Wait for an interrupt (idle loop)
+        }
+    }
+}
